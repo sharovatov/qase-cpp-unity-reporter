651b7b09e293741355922973b3cd21a61e247edb
Vitaly Sharovatov <vitaly.sharovatov@gmail.com>
2025-04-29 17:20:51 +0200

    red: qase reporter should store at least one test result

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -6,9 +6,14 @@ using namespace qase;
 
 int main()
 {
-
 	// qase reporter should be able to accept test execution result
 	qase_reporter_add_result("MyFirstTest", true);
+
+	// qase reporter should also be able to store the test execution result
+	const auto& results = qase_reporter_get_results();
+	assert(results.size() == 1);
+    assert(results[0].name == "StoreTest");
+    assert(results[0].passed == true);
 
 	std::cout << "All TDD checks passed!" << std::endl;
 
