2b9f5f37e6975a20c595ebbdec930443e40d2321
Vitaly Sharovatov <vitaly.sharovatov@gmail.com>
2025-04-29 18:40:28 +0200

    red: results are always persisted once sent

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -4,9 +4,9 @@
 
 using namespace qase;
 
+// qase reporter should be able to accept test execution result and store it
 void test_results_accepted_stored()
 {
-	// qase reporter should be able to accept test execution result and store it
 	qase_reporter_add_result("MyFirstTest", true);
 
 	const auto& results = qase_reporter_get_results();
@@ -16,10 +16,18 @@ void test_results_accepted_stored()
 
 }
 
+// if nothing is added to the reporter, it doesn't die but returns empty vector
+void test_results_are_empty_when_none_added() {
+    const auto& results = qase_reporter_get_results();
+    assert(results.empty());
+}
+
+
 int main()
 {
-
 	test_results_accepted_stored();
+	test_results_are_empty_when_none_added();
+
 
 	std::cout << "All TDD checks passed!" << std::endl;
 
