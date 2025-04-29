96b34e65694f3102f2745251334a1276c7908cbb
Vitaly Sharovatov <vitaly.sharovatov@gmail.com>
2025-04-29 17:37:05 +0200

    green: qase reporter stores and retrieves single test result

--- include/qase_reporter.h
+++ include/qase_reporter.h
@@ -1,9 +1,16 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace qase {
 
+	struct TestResult {
+		std::string name;
+		bool passed;
+	};
+
 	void qase_reporter_add_result(const std::string& name, bool passed);
+	const std::vector<TestResult>& qase_reporter_get_results();
 
 }
--- src/qase_reporter.cpp
+++ src/qase_reporter.cpp
@@ -2,8 +2,14 @@
 
 namespace qase {
 
-	void qase_reporter_add_result(const std::string& name, bool passed) {
+	static std::vector<TestResult> collected;
 
+	void qase_reporter_add_result(const std::string& name, bool passed) {
+		collected.push_back(TestResult{name, passed});
+	}
+
+	const std::vector<TestResult>& qase_reporter_get_results() {
+		return collected;
 	}
 
 }
