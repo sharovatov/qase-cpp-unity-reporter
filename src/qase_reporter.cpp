b8b900b526271eeae1d6c12b0c59360253610c92
Vitaly Sharovatov <vitaly.sharovatov@gmail.com>
2025-05-01 15:14:43 +0200

    green: if no test name is passed, it throws

--- src/qase_reporter.cpp
+++ src/qase_reporter.cpp
@@ -5,6 +5,9 @@ namespace qase {
 	static std::vector<TestResult> collected;
 
 	void qase_reporter_add_result(const std::string& name, bool passed) {
+		if (name.empty()) {
+			throw std::invalid_argument("Test name must not be empty");
+		}
 		collected.push_back(TestResult{name, passed});
 	}
 
